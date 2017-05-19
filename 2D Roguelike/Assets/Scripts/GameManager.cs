fc4b5f6330c54b5d3f9e844dd3817bfabb6ed2cd
wl9001180 <Canada_wl@aliyun.com>
2017-05-18 23:40:35 -0400

    2D Roguelike Done

--- 2D Roguelike/Assets/Scripts/GameManager.cs
+++ 2D Roguelike/Assets/Scripts/GameManager.cs
@@ -1,38 +1,113 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+using System.Collections.Generic;       
 
-    public static GameManager instance = null;
+public class GameManager : MonoBehaviour
+{
+    public float levelStartDelay = 2f;                   
+    public float turnDelay = 0.1f;                          
+    public int playerFoodPoints = 100;                     
+    public static GameManager instance = null;              
+    [HideInInspector] public bool playersTurn = true;
 
-    public BoardManager boardScript;
+    private Text levelText;
+    private GameObject levelImage;
 
-    private int level = 3;
+    private BoardManager boardScript;                     
+    private int level = 1;                            
+    private List<Enemy> enemies;                        
+    private bool enemiesMoving;
+    private bool doingSetup;
 
-
-	// Use this for initialization
-	void Awake () {
+    void Awake()
+    {
         if (instance == null)
-        {
+
             instance = this;
-        } else if(instance != this){
+
+        else if (instance != this)
+
             Destroy(gameObject);
-        }
 
         DontDestroyOnLoad(gameObject);
+
+        enemies = new List<Enemy>();
+
         boardScript = GetComponent<BoardManager>();
+
         InitGame();
-	}
-	
-    void InitGame()
-    {
-        boardScript.SetupScene(level);
     }
 
+    void OnLevelWasLoaded(int index)
+    {
+        level++;
+        InitGame();
+    }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void InitGame()
+    {
+        doingSetup = true;
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+
+        enemies.Clear();
+        boardScript.SetupScene(level);
+
+    }
+
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+    void Update()
+    {
+        if (playersTurn || enemiesMoving || doingSetup)
+
+            return;
+
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    public void GameOver()
+    {
+
+        levelText.text = "After " + level + " days, you starved.";
+        levelImage.SetActive(true);
+
+        enabled = false;
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+
+        yield return new WaitForSeconds(turnDelay);
+
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+        playersTurn = true;
+
+        enemiesMoving = false;
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Unity-Learn
+
+2D Roguelike
+	https://unity3d.com/cn/learn/tutorials/projects/2d-roguelike-tutorial
+
+	movement
+	sounds play
+	Animator
+	
+	need to learn:
+		How to make Sprite
+
+
